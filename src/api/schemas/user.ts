6611523a1b101128fbc5bf0f7302b1ffa6f9906c
Yuze.Ma <584653629@qq.com>
2018-05-03 01:01:43 -0400

    Trying to not use default export

---
+++ src/api/schemas/user.ts
@@ -0,0 +1,27 @@
+import { Document, Schema, Model, model } from 'mongoose';
+import { User } from '../interfaces/user';
+
+export interface UserModel extends User, Document {
+    fullname() : string;
+}
+
+export var UserSchema:Schema = new Schema({
+    createdAt : Date,
+    email : String,
+    firstName : String,
+    lastName : String,
+});
+
+UserSchema.pre(`save`, (next) => {
+    const now = new Date();
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
+});
+
+UserSchema.methods.fullName = function() : string {
+    return(this.firstName.trim() + ` ` + this.lastName.trim());
+};
+
+export const UserModel:Model<UserModel> = model<UserModel>(`User`, UserSchema);
